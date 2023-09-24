8acbc6c5953afeb24c98e40da3d66ec230c88cd8
Patryk Makulski <patryk.makulski@interia.pl>
2023-09-24 20:23:04 +0200

    Added: Displayed daily.

--- src/components/DetailsNutrition.tsx
+++ src/components/DetailsNutrition.tsx
@@ -12,23 +12,33 @@ const DetailsNutrition = ({ testNut }: Props) => {
     <List alignItems="flex-start" w="100%">
       {testNut.map((item) =>
         Object.prototype.hasOwnProperty.call(item, "sub") ? (
-          <div key={item.label + "3"}>
-            <HStack justifyContent="space-between" key={item.label + "1"}>
+          <div key={item.label + "1"}>
+            <HStack justifyContent="space-between">
               <ListItem padding="5px 0" key={item.label}>
                 {item.label}
               </ListItem>
-              <ListItem key={item.total}>
-                {Math.round(item.total) + item.unit}
-              </ListItem>
+              <HStack>
+                <ListItem key={item.total}>
+                  {Math.round(item.total) + item.unit}
+                </ListItem>
+                <ListItem key={item.daily}>
+                  {Math.round(item.daily) + "%"}
+                </ListItem>
+              </HStack>
             </HStack>
             {item.sub.map((s) => (
               <HStack justifyContent="space-between" key={s.label + "1"}>
                 <ListItem key={s.label} padding="5px 5px 5px 20px">
                   {s.label}
                 </ListItem>
-                <ListItem key={s.total}>
-                  {Math.round(s.total) + item.unit}
-                </ListItem>
+                <HStack>
+                  <ListItem key={s.total}>
+                    {Math.round(s.total) + item.unit}
+                  </ListItem>
+                  <ListItem key={s.daily + "1"}>
+                    {Math.round(s.daily) + "%"}
+                  </ListItem>
+                </HStack>
               </HStack>
             ))}
           </div>
@@ -37,9 +47,14 @@ const DetailsNutrition = ({ testNut }: Props) => {
             <ListItem padding="5px 0" key={item.label}>
               {item.label}
             </ListItem>
-            <ListItem key={item.total}>
-              {Math.round(item.total) + item.unit}
-            </ListItem>
+            <HStack>
+              <ListItem key={item.total}>
+                {Math.round(item.total) + item.unit}
+              </ListItem>
+              <ListItem key={item.total + 1}>
+                {Math.round(item.daily) + "%"}
+              </ListItem>
+            </HStack>
           </HStack>
         )
       )}
@@ -49,42 +64,55 @@ const DetailsNutrition = ({ testNut }: Props) => {
 
 export default DetailsNutrition;
 
-// {(Object.keys(totalNutritients) as (keyof typeof totalNutritients)[]).map(
-//     (key) =>
-//       key !== String("ENERC_KCAL") &&
-//       key !== String("FASAT") &&
-//       key !== String("FATRN") &&
-//       key !== String("FAMS") &&
-//       key !== String("FAPU") &&
-//       key !== String("CHOCDF.net") &&
-//       key !== String("FIBTG") &&
-//       key !== String("SUGAR") ? (
-//         <HStack justifyContent="space-between">
-//           <ListItem padding="5px 0" key={key}>
-//             {totalNutritients[key].label}
+// <List alignItems="flex-start" w="100%">
+// {testNut.map((item) =>
+//   Object.prototype.hasOwnProperty.call(item, "sub") ? (
+//     <div key={item.label + "1"}>
+//       <HStack justifyContent="space-between">
+//         <ListItem padding="5px 0" key={item.label}>
+//           {item.label}
+//         </ListItem>
+//         <HStack>
+//           <ListItem key={item.total}>
+//             {Math.round(item.total) + item.unit}
 //           </ListItem>
-
-//           {/* <ListItem key={key}>
-//               {Math.round(totalNutritients[key].quantity) +
-//                 totalNutritients[key].unit}
-//             </ListItem> */}
-//           {/* <ListItem key={key}>
-//             {totalDaily[key].quantity + totalDaily[key].unit}
-//           </ListItem> */}
-//         </HStack>
-//       ) : key !== String("ENERC_KCAL") ? (
-//         <HStack justifyContent="space-between">
-//           <ListItem padding="5px 5px 5px 20px" key={key}>
-//             {totalNutritients[key].label}
+//           <ListItem key={item.daily}>
+//             {Math.round(item.daily) + "%"}
 //           </ListItem>
 //         </HStack>
-//       ) : null
-//   )}
-
-// key !== String("ENERC_KCAL") ? (
-//     <ListItem key={key}>{totalNutritients[key].label}</ListItem>
+//       </HStack>
+//       {item.sub.map((s) => (
+//         <HStack justifyContent="space-between" key={s.label + "1"}>
+//           <ListItem key={s.label} padding="5px 5px 5px 20px">
+//             {s.label}
+//           </ListItem>
+//           <HStack>
+//             <ListItem key={s.total}>
+//               {Math.round(s.total) + item.unit}
+//             </ListItem>
+//             <ListItem key={s.daily + "1"}>
+//               {Math.round(s.daily) + "%"}
+//             </ListItem>
+//           </HStack>
+//         </HStack>
+//       ))}
+//     </div>
 //   ) : (
-//     <ListItem padding={10} key={key}>
-//       {totalNutritients[key].label}
-//     </ListItem>
+//     <HStack justifyContent="space-between" key={item.label + "2"}>
+//       <ListItem padding="5px 0" key={item.label}>
+//         {item.label}
+//       </ListItem>
+//       <HStack>
+//         <ListItem key={item.total}>
+//           {Math.round(item.total) + item.unit}
+//         </ListItem>
+//         <ListItem key={item.total + 1}>
+//           {Math.round(item.daily) + "%"}
+//         </ListItem>
+//       </HStack>
+//     </HStack>
 //   )
+// )}
+// </List>
+// );
+// };
