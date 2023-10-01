392f4f552730feb7a751536c15328c77af30b4d0
Patryk Makulski <patryk.makulski@interia.pl>
2023-10-01 16:19:25 +0200

    Added: Back page button

--- src/components/media/Logo.tsx
+++ src/components/media/Logo.tsx
@@ -1,15 +1,19 @@
-import { HStack, Image, Show } from "@chakra-ui/react";
+import { HStack, Image, Link, Show } from "@chakra-ui/react";
 import logo from "../../assets/logo.png";
 import smallLogo from "../../assets/small-logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Logo = () => {
+  const navigate = useNavigate();
   return (
-    <HStack paddingLeft="20px">
-      <Image src={smallLogo}></Image>
-      <Show above="sm">
-        <Image paddingLeft="2px" src={logo}></Image>
-      </Show>
-    </HStack>
+    <Link onClick={() => navigate(-1)}>
+      <HStack paddingLeft="20px">
+        <Image src={smallLogo}></Image>
+        <Show above="sm">
+          <Image paddingLeft="2px" src={logo}></Image>
+        </Show>
+      </HStack>
+    </Link>
   );
 };
 
